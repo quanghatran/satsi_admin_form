8ed4003d182413a252b23b3f16644a2e28729f3a
quanghatran <quanghatran2161998@gmail.com>
2021-07-22 11:59:51 +0700

    adding launcher filed

--- src/pages/Satsi/RegisterProgram.js
+++ src/pages/Satsi/RegisterProgram.js
@@ -1,104 +1,116 @@
-import { format, parseISO } from 'date-fns';
-import TableData from '../../components/TableData';
+import { format, parseISO } from "date-fns";
+import TableData from "../../components/TableData";
 
 const RegisterProgram = () => {
-  const tableApi = process.env.REACT_APP_API_URL_S + '/Contact';
+	const tableApi = process.env.REACT_APP_API_URL + "Contact";
 
-  const columns = [
-    {
-      field: 'id',
-      headerName: 'ID',
-      sortable: false,
-      width: 100,
-    },
-    {
-      field: 'Name',
-      headerName: 'Tên',
-      sortable: false,
-      width: 200,
-    },
-    {
-      field: 'PhoneNumber',
-      headerName: 'Số điện thoại',
-      sortable: false,
-      width: 200,
-    },
-    {
-      field: 'Email',
-      headerName: 'Email',
-      sortable: false,
-      width: 300,
-    },
-    {
-      field: 'Service',
-      headerName: 'Dịch vụ quan tâm',
-      sortable: false,
-      width: 300,
-    },
-    {
-      field: 'SendDay',
-      headerName: 'Ngày đăng ký',
-      sortable: false,
-      type: Date,
-      width: 200,
-    },
-  ];
+	const columns = [
+		{
+			field: "id",
+			headerName: "ID",
+			sortable: false,
+			width: 100,
+		},
+		{
+			field: "Name",
+			headerName: "Tên",
+			sortable: false,
+			width: 200,
+		},
+		{
+			field: "PhoneNumber",
+			headerName: "Số điện thoại",
+			sortable: false,
+			width: 200,
+		},
+		{
+			field: "Email",
+			headerName: "Email",
+			sortable: false,
+			width: 300,
+		},
+		{
+			field: "Service",
+			headerName: "Dịch vụ quan tâm",
+			sortable: false,
+			width: 300,
+		},
+		{
+			field: "Launcher",
+			headerName: "Người giới thiệu",
+			sortable: false,
+			width: 200,
+		},
+		{
+			field: "SendDay",
+			headerName: "Ngày đăng ký",
+			sortable: false,
+			type: Date,
+			width: 200,
+		},
+	];
 
-  const headersExcel = [
-    {
-      label: 'id',
-      key: 'ID',
-    },
-    {
-      label: 'Name',
-      key: 'Tên',
-    },
-    {
-      label: 'PhoneNumber',
-      key: 'Số điện thoại',
-    },
-    {
-      label: 'Email',
-      key: 'Email',
-    },
-    { label: 'Service', key: 'Dịch vụ quan tâm' },
-    {
-      label: 'SendDay',
-      key: 'Ngày đăng ký',
-    },
-  ];
+	const headersExcel = [
+		{
+			label: "id",
+			key: "ID",
+		},
+		{
+			label: "Name",
+			key: "Tên",
+		},
+		{
+			label: "PhoneNumber",
+			key: "Số điện thoại",
+		},
+		{
+			label: "Email",
+			key: "Email",
+		},
 
-  const titleTable = 'Danh sách tham gia chương trình';
+		{ label: "Service", key: "Dịch vụ quan tâm" },
+		{
+			label: "Launcher",
+			key: "Người giới thiệu",
+		},
+		{
+			label: "SendDay",
+			key: "Ngày đăng ký",
+		},
+	];
 
-  const filename = 'danh_sach_tham_gia_chương trình.csv';
+	const titleTable = "Danh sách tham gia chương trình";
 
-  const createData = (notes) => {
-    const rows = notes
-      .filter((data) => data.status === 0)
-      .map(({ id, Name, PhoneNumber, Email, Service, SendDay }) => {
-        return {
-          id,
-          Name,
-          PhoneNumber,
-          Email,
-          Service,
-          SendDay: format(parseISO(SendDay), "h:m a '-' dd/MM/yyyy"),
-        };
-      });
-    return rows;
-  };
+	const filename = "danh_sach_tham_gia_chương trình.csv";
 
-  return (
-    <TableData
-      columns={columns}
-      createData={createData}
-      headersExcel={headersExcel}
-      tableApi={tableApi}
-      titleTable={titleTable}
-      filename={filename}
-      isRemove={0}
-    />
-  );
+	const createData = (notes) => {
+		const rows = notes
+			.filter((data) => data.status === 0)
+			.map(({ id, Name, PhoneNumber, Email, Service, Launcher, SendDay }) => {
+				return {
+					id,
+					Name,
+					PhoneNumber,
+					Email,
+					Service,
+					Launcher,
+					SendDay: format(parseISO(SendDay), "h:m a '-' dd/MM/yyyy"),
+				};
+			});
+		return rows;
+	};
+
+	return (
+		<TableData
+			columns={columns}
+			createData={createData}
+			headersExcel={headersExcel}
+			tableApi={tableApi}
+			titleTable={titleTable}
+			filename={filename}
+			isRemove={0}
+		/>
+	);
 };
 
 export default RegisterProgram;
